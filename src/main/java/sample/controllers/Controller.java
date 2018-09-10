2788c9892b3e0f4b55becb84be2d8427cfb09b60
Pavel <saymeales@gmail.com>
2018-09-10 19:43:20 +0300

    add alert

--- src/main/java/sample/controllers/Controller.java
+++ src/main/java/sample/controllers/Controller.java
@@ -9,7 +9,6 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import org.fxmisc.flowless.VirtualizedScrollPane;
 import org.fxmisc.richtext.CodeArea;
-import sample.exceptions.LoadException;
 import sample.exceptions.NothingFindException;
 import sample.filework.PathGetter;
 import sample.filework.SearchElement;
@@ -66,9 +65,9 @@ public class Controller {
                     PathGetter pathGetter = null;
                     try {
                         pathGetter = new StringPathGetter(folderField.getText());
-                        alert(new LoadException("Loading..."));
+                        alert("Loading...");
                         searchResults = SearchHelper.searchAllPosInFolder(pathGetter.getPath(),searchField.getText(),extensionField.getText());
-                        alert(new LoadException("Loaded!"));
+                        alert("Loaded!");
                         Set<Path> files = searchResults.keySet();
                         getTreeNodes(pathGetter.getPath(), files);
                         resultTree.setRoot(getTreeNodes(pathGetter.getPath(),files));
@@ -77,7 +76,7 @@ public class Controller {
                         alert(uriE);
                     }
                     catch (Error e) {
-                        alertError(e);
+                        alert(e);
                     }
                 });
                 return null;
@@ -188,16 +187,20 @@ public class Controller {
         if (e instanceof NothingFindException)
             alert.setContentText("Nothing was finded");
         else
-        if (e instanceof LoadException)
-            alert.setContentText(e.getMessage());
-        else
             alert.setContentText("Some more troubles " + e.getMessage());
         alert.getDialogPane().setPrefSize(400, 100);
         alert.showAndWait();
     }
 
-    private void alertError(Error e){
+    private void alert(Error e){
         Alert alert = new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK);
+        alert.getDialogPane().setPrefSize(400, 100);
+        alert.showAndWait();
+    }
+
+
+    private void alert(String message){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, message, ButtonType.OK);
         alert.getDialogPane().setPrefSize(400, 100);
         alert.showAndWait();
     }
